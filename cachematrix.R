df3c9b615b3271fc42ad169943b5c82edd253dad
Mark Thompson <mark@ruthless.ws>
2017-02-18 09:37:53 +1300

    Comments describing functions added

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions will create a matrix which can have the inverse
+## calculated of them and store them in the cache.
 
-## Write a short comment describing this function
+## This function will check to see if a matrix already exists in the 
+## cache and return this, else if there is no matrix in the cache then
+## it will create one.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This will return a matrix which is the inverse of object x if x is
+## not an inverse matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
