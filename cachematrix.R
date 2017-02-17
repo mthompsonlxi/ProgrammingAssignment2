40bcb454fc169a065bd425bb1c8dcbb49f6a0f94
Mark Thompson <mark@ruthless.ws>
2017-02-18 12:49:43 +1300

    cacheSolve function written and tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,28 @@
 ## These two functions will create a matrix which can have the inverse
 ## calculated of them and store them in the cache.
 
-## This function will check to see if a matrix already exists in the 
-## cache and return this, else if there is no matrix in the cache then
-## it will create one.
+## The makeCacheMatrix function checks the matrix object specified to
+## verify that it is square and if no object is specified it will check
+## to see if there is one in the cache
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## This will return a matrix which is the inverse of object x if x is
-## not an inverse matrix
+## The cacheSolve() function returns the inverse of the matrix specified 
+## in the arguments UNLESS an existing inverse matrix has already been
+## saved in the global environment.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+      if(exists("m_inverse")) {
+            message("Existing Inverse Matrix in cache, displaying this instead \nof specified Square Matrix. Saved as \"m_inverse\"")
+            return(m_inverse)
+      }
+      
+      m_inverse <<- solve(x)
+      message("Matrix saved as \"m_inverse\"")
+      m_inverse
+
 }
